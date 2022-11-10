bbf7916e583153b4a4e0465bd0206e807c9fd7e2
gnahc2305 <achangmurillo@gmail.com>
2022-11-10 07:01:19 -0500

    change to function components

--- src/App.js
+++ src/App.js
@@ -1,86 +1,159 @@
-import React, { Component } from "react";
+import React, { useState, useRef } from "react";
 import Info from "./components/info";
 import Experience from "./components/experience";
 import Education from "./components/education";
 import "./style.css";
 
-class App extends Component {
-  constructor() {
-    super();
+const App = () => {
+  const [info, setInfo] = useState({});
+  const nameRef = useRef();
 
-    this.information = {
-      name: "",
-      title: "",
-      address: "",
-      phone: "",
-      email: "",
-      description: "",
-    };
-
-    this.experience = {
-      position: "",
-      company: "",
-      from: "",
-      to: "",
-    };
-
-    this.education = {
-      university: "",
-      city: "",
-      degree: "",
-    };
-  }
-
-  setInformation = (e) => {
+  function submitInfo(e) {
     e.preventDefault();
-    console.log(e.target);
-  };
+    const info = nameRef.current.value;
 
-  render() {
-    return (
-      <div>
-        <div className="info">
-          <form>
-            <h2>Personal Information</h2>
-            <input type="text" placeholder="Full Name"></input>
-            <input type="text" placeholder="Title"></input>
-            <input type="text" placeholder="Address"></input>
-            <input type="text" placeholder="Phone Number"></input>
-            <input type="text" placeholder="Email"></input>
-            <textarea placeholder="Description" rows="5" cols="30"></textarea>
-            <button onClick={this.setInformation}>Submit</button>
-          </form>
-        </div>
+    // setInfo({
 
-        <div>
-          <form>
-            <h2>Experience</h2>
-            <input type="text" placeholder="Position"></input>
-            <input type="text" placeholder="Company"></input>
-            <input type="text" placeholder="From"></input>
-            <input type="text" placeholder="To"></input>
-            <button>Submit</button>
-          </form>
-        </div>
-
-        <div>
-          <form>
-            <h2>Education</h2>
-            <input type="text" placeholder="University"></input>
-            <input type="text" placeholder="City"></input>
-            <input type="text" placeholder="Degree"></input>
-            <button>Submit</button>
-          </form>
-        </div>
-
-        <div className="cv">
-          <Info></Info>
-          <Experience></Experience>
-          <Education></Education>
-        </div>
-      </div>
-    );
+    // })
   }
-}
+
+  return (
+    <div>
+      <div className="info">
+        <form>
+          <h2>Personal Information</h2>
+          <input
+            type="text"
+            placeholder="Full Name"
+            ref={nameRef}
+          ></input>
+          <input type="text" placeholder="Title"></input>
+          <input type="text" placeholder="Address"></input>
+          <input type="text" placeholder="Phone Number"></input>
+          <input type="text" placeholder="Email"></input>
+          <textarea placeholder="Description" rows="5" cols="30"></textarea>
+          <button onClick={submitInfo}>Submit</button>
+        </form>
+      </div>
+
+      <div>
+        <form>
+          <h2>Experience</h2>
+          <input type="text" placeholder="Position"></input>
+          <input type="text" placeholder="Company"></input>
+          <input type="text" placeholder="From"></input>
+          <input type="text" placeholder="To"></input>
+          <button>Submit</button>
+        </form>
+      </div>
+
+      <div>
+        <form>
+          <h2>Education</h2>
+          <input type="text" placeholder="University"></input>
+          <input type="text" placeholder="City"></input>
+          <input type="text" placeholder="Degree"></input>
+          <button>Submit</button>
+        </form>
+      </div>
+
+      <div className="cv">
+        <Info></Info>
+        <Experience></Experience>
+        <Education></Education>
+      </div>
+    </div>
+  );
+};
 
 export default App;
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.information = {
+//       name: "",
+//       title: "",
+//       address: "",
+//       phone: "",
+//       email: "",
+//       description: "",
+//     };
+
+//     this.experience = {
+//       position: "",
+//       company: "",
+//       from: "",
+//       to: "",
+//     };
+
+//     this.education = {
+//       university: "",
+//       city: "",
+//       degree: "",
+//     };
+
+//   }
+
+//   handleChange = (e) => {
+//     this.setState({
+//       name: e.target.value,
+//     });
+//   };
+
+//   setInformation = (e) => {
+//     e.preventDefault();
+//     console.log(this.state.name);
+//   };
+
+//   render() {
+//     return (
+//       <div>
+//         <div className="info">
+//           <form>
+//             <h2>Personal Information</h2>
+//             <input
+//               type="text"
+//               placeholder="Full Name"
+//               onChange={this.handleChange}
+//               value={this.name}
+//             ></input>
+//             <input type="text" placeholder="Title"></input>
+//             <input type="text" placeholder="Address"></input>
+//             <input type="text" placeholder="Phone Number"></input>
+//             <input type="text" placeholder="Email"></input>
+//             <textarea placeholder="Description" rows="5" cols="30"></textarea>
+//             <button onClick={this.setInformation}>Submit</button>
+//           </form>
+//         </div>
+
+//         <div>
+//           <form>
+//             <h2>Experience</h2>
+//             <input type="text" placeholder="Position"></input>
+//             <input type="text" placeholder="Company"></input>
+//             <input type="text" placeholder="From"></input>
+//             <input type="text" placeholder="To"></input>
+//             <button>Submit</button>
+//           </form>
+//         </div>
+
+//         <div>
+//           <form>
+//             <h2>Education</h2>
+//             <input type="text" placeholder="University"></input>
+//             <input type="text" placeholder="City"></input>
+//             <input type="text" placeholder="Degree"></input>
+//             <button>Submit</button>
+//           </form>
+//         </div>
+
+//         <div className="cv">
+//           <Info name={this.information.name}></Info>
+//           <Experience></Experience>
+//           <Education></Education>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
--- src/components/info.js
+++ src/components/info.js
@@ -1,26 +1,21 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
+import React from "react";
 
+const Info = (props) => {
+  const { name } = props;
 
-class Info extends Component {
-    constructor(prop) {
-        super(prop)
-    }
-
-
-    render() {
-        return (
-            <div>
-                <h1>Fulll Name</h1>
-                <ul>
-                    <p>Title</p>
-                    <p>Address</p>
-                    <p>Number</p>
-                    <p>Email</p>
-                    <p>Description</p>
-                </ul>
-            </div>
-        )
-    }
-}
+  return (
+    <div>
+      <h1>{name}</h1>
+      <ul>
+        <p>Title</p>
+        <p>Address</p>
+        <p>Number</p>
+        <p>Email</p>
+        <p>Description</p>
+      </ul>
+    </div>
+  );
+};
 
 export default Info
